808a4be9976a01e5b15cdf884a25e73db5cd2de9
sisily11 <74577226+sisily11@users.noreply.github.com>
2022-02-09 16:15:06 +0900

    11
    11

---
+++ oral_fit/lib/app/core/theme/theme_data.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final primaryColor = Color(0xFF249EE3);
+final primaryLiteColor = Color(0xFFEDF8FF);
+final primaryDarkColor = Color(0xFF003757);
+final borderBoxColor = Color(0xFF249EE3);
+final selectFontColor = Color(0xFF0B82C6);
+
+final greyBoxColor = Color(0xFFDF8F8F8);
+final greyLiteColor = Color(0xFFDDDDDD);
+final greySubLiteColor = Color(0xFF999999);
+final greyDarkColor = Color(0xFF555555);
+final greySubDarkColor = Color(0xFF777777);
+
+final primaryFontColor = Color(0xFF222222);
+
+final primaryFont = "Noto_Serif_KR";
--- oral_fit/lib/main.dart
+++ oral_fit/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:oral_fit/app/core/theme/theme_data.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "ORAL FIT",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData(fontFamily: primaryFont, primaryColor: primaryFontColor),
     ),
   );
 }
