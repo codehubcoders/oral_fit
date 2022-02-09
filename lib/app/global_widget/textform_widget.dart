dc9c8cc1394e291c46c45c5d6e97d39a04233776
sisily11 <74577226+sisily11@users.noreply.github.com>
2022-02-10 08:50:27 +0900

    11

--- lib/app/global_widget/textform_widget.dart
+++ lib/app/global_widget/textform_widget.dart
@@ -3,12 +3,29 @@ import 'package:flutter/material.dart';
 import '../core/theme/theme_data.dart';
 
 class TextFormWidget extends StatelessWidget {
+  final TextEditingController inputController;
+  final String hint;
+  final FormFieldSetter onSaved;
+  final FormFieldValidator validator;
+  final TextInputType? keyboardType;
   final String hitText;
-  const TextFormWidget({Key? key, required this.hitText}) : super(key: key);
+
+  const TextFormWidget(
+      {Key? key,
+      required this.hitText,
+      required this.inputController,
+      required this.hint,
+      required this.onSaved,
+      required this.validator,
+      this.keyboardType})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+        validator: validator,
+        onSaved: onSaved,
+        controller: inputController,
         decoration: InputDecoration(
             hintStyle: TextStyle(fontSize: 13),
             contentPadding: const EdgeInsets.fromLTRB(20, 15, 10, 15),
